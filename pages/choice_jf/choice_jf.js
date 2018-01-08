6c5973dc0f557369e8bcf103ed000d02fef05d94
huyanqi <15212786683@163.com>
2018-01-08 13:25:13 +0800

    确认订单
    下一步，提交订单调支付

--- pages/choice_jf/choice_jf.js
+++ pages/choice_jf/choice_jf.js
@@ -7,7 +7,8 @@ Page({
     BillResult: [],
     BillAmount: [],
     amountSum: 0.00,
-    isChoose: ["circle", "success"]
+    isChoose: [],
+    isChooseAll: 'circle'
   },
   onLoad: function () {
     this.setData({
@@ -27,15 +28,22 @@ Page({
         if (res.data.status === 'ok') {
           var result = JSON.parse(res.data.Result)
           var bill_amount = new Array()
+          var is_choose = new Array()
           console.log(result)
           for (var i = 0; i < result.length; i++) {
             bill_amount.push(result[i].BillAmount)
+            is_choose.push('circle')
             that.setData({
-              BillAmount: bill_amount
+              BillAmount: bill_amount,
+              isChoose: is_choose
             })
           }
           that.setData({
-            BillResult: result
+            BillResult: result,
+          })
+          wx.setStorage({
+            key: 'BillResult',
+            data: result,
           })
         } else {
           console.log('GetAllBill接口异常')
@@ -43,59 +51,87 @@ Page({
       }
     })
   },
-  onReady: function () {
-    // 页面渲染完成
-  },
-  onShow: function () {
-    // 页面显示
-  },
   chooseClick: function (e) {
-    this.setData({
-      isChoose: this.data.isChoose.reverse()
-    })
-    var sum = 0;
-    for (var j = 0; j < this.data.BillAmount.length; j++) {
-      sum = sum + this.data.BillAmount[j]
+    var index = e.currentTarget.dataset.index
+    var choose_key = 'isChoose[' + index + ']'
+    var sum1 = this.data.amountSum + this.data.BillAmount[index]
+    var sum2 = this.data.amountSum - this.data.BillAmount[index]
+    if (e.currentTarget.dataset.ischoose === 'circle') {
       this.setData({
-        amountSum: sum
+        [choose_key]: 'success',
+        amountSum: sum1
+      })
+      if (this.data.isChoose.indexOf('circle') === -1) {
+        this.setData({
+          isChooseAll: 'success'
+        })
+      }
+    } else if (e.currentTarget.dataset.ischoose === 'success') {
+      this.setData({
+        [choose_key]: "circle",
+        amountSum: sum2,
+        isChooseAll: 'circle'
       })
     }
   },
   chooseAllClick: function (e) {
-    var sum = 0;
-    for (var j = 0; j < this.data.BillAmount.length; j++) {
-      sum = sum + this.data.BillAmount[j]
+    var sum = 0
+    if (e.currentTarget.dataset.ischooseall === 'circle') {
       this.setData({
-        amountSum: sum
+        isChooseAll: 'success'
+      })
+      for (var i = 0; i < this.data.isChoose.length; i++) {
+        var key = 'isChoose[' + i + ']'
+        this.setData({
+          [key]: 'success'
+        })
+      }
+      for (var j = 0; j < this.data.BillAmount.length; j++) {
+        sum = sum + this.data.BillAmount[j]
+        this.setData({
+          amountSum: sum
+        })
+      }
+    } else if (e.currentTarget.dataset.ischooseall === 'success') {
+      this.setData({
+        isChooseAll: "circle",
+        amountSum: 0.00
+      })
+      for (var i = 0; i < this.data.isChoose.length; i++) {
+        var key = 'isChoose[' + i + ']'
+        this.setData({
+          [key]: 'circle'
+        })
+      }
+    }
+  },
+  bindSubmit: function () {
+    if (this.data.isChoose.indexOf('success') === -1) {
+      wx.showModal({
+        title: '提示',
+        content: '请先选择账单',
+        showCancel: false,
+        confirmColor: '#3c95fe'
+      })
+    } else {
+      var sel_arr = [], sel_index = [], sel_bill = []
+      while (this.data.isChoose.indexOf('success', sel_arr.length) !== -1) {
+        sel_arr.push(this.data.isChoose.indexOf('success', sel_arr.length))
+      }
+      for (var i = 0; i < sel_arr.length; i++) {
+        if (sel_arr[i + 1] === sel_arr[i]) continue
+        else {
+          sel_index.push(sel_arr[i])
+        }
+      }
+      for (var j = 0; j < sel_index.length; j++) {
+        sel_bill.push(this.data.BillResult[sel_index[j]])
+      }
+      console.log(sel_bill)
+      wx.setStorageSync('sel_bill', sel_bill)
+      wx.navigateTo({
+        url: '../dd-submit/dd-submit',
       })
     }
-    // if (this.data.isChooseAll == "success") {
-    //   this.data.isChooseArr.forEach(function (e) {
-    //     this.setData({
-    //       e: "circle"
-    //     })
-    //   })
-    //   this.setData({
-    //     isChooseAll: "circle"
-    //   })
-    // } else if (this.data.isChooseAll == "circle") {
-    //   this.data.isChooseArr.forEach(function (e) {
-    //     this.setData({
-    //       e: "success"
-    //     })
-    //   })
-    //   this.setData({
-    //     isChooseAll: "success"
-    //   })
-    // }
-  },
-  chooseAllClick: function (e) {
-    // this.data.isChooseArr.forEach(function (item) {
-    //   if (this.data.isChooseAll == "success") {
-    //     this.setData({ isChooseAll: "circle" })
-    //   } else if (this.data.isChooseAll == "circle") {
-    //     this.setData({ isChooseAll: "success" })
-    //   }
-    // })
   }
 })
--- pages/dd-submit/dd-submit.js
+++ pages/dd-submit/dd-submit.js
@@ -1,19 +1,57 @@
 // pages/dd-submit/dd-submit.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    PartyName: '',
+    CompanyName: '',
+    PartyMemberName: '',
+    BillList: [],
+    SubBillList: '',
+    AmountSum: 0
   },
-  onReady:function(){
-    // 页面渲染完成
+  onLoad: function () {
+    this.setData({
+      PartyName: wx.getStorageSync('PartyName'),
+      CompanyName: wx.getStorageSync('CompanyName'),
+      PartyMemberName: wx.getStorageSync('PartyMemberName'),
+      BillList: wx.getStorageSync('sel_bill')
+    })
+    var sub_bill_list = []
+    for (var index = 0; index < this.data.BillList.length; index++) {
+      sub_bill_list[index] = new Object()
+      sub_bill_list[index].BillSysNo = this.data.BillList[index].BillSysNo
+      sub_bill_list[index].BillAmount = this.data.BillList[index].BillAmount
+      sub_bill_list[index].BillName = this.data.BillList[index].BillName
+      sub_bill_list[index].BillType = this.data.BillList[index].BillType
+      sub_bill_list[index].ChargeItemName = this.data.BillList[index].ChargeItemName
+      sub_bill_list[index].ChargeItemSysNo = this.data.BillList[index].ChargeItemSysNo
+      sub_bill_list[index].PartyMemberSysNo = this.data.BillList[index].PartyMemberSysNo
+      sub_bill_list[index].MerchantSysNo = this.data.BillList[index].MerchantSysNo
+      sub_bill_list[index].OrganSysNo = this.data.BillList[index].OrganSysNo
+    }
+    this.setData({
+      SubBillList: JSON.stringify(sub_bill_list)
+    })
+    var amount_sum = 0
+    for (var i = 0; i < this.data.BillList.length; i++) {
+      amount_sum = amount_sum + this.data.BillList[i].BillAmount
+    }
+    this.setData({
+      AmountSum: amount_sum
+    })
   },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
+  submitBill: function () {
+    var that = this
+    wx.request({
+      url: 'https://test99.allinpaymall.com/PartyTest/Bill.aspx',
+      data: {
+        Method: 'SubmitBill',
+        BillAddress: that.data.PartyName + ' ' + that.data.CompanyName + '-' + that.data.PartyMemberName,
+        BillData: that.data.SubBillList
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res.data.Result)
+      }
+    })
   }
 })
